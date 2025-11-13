9fd5350c2bd7cddf4759ffbb1f5c14aa2c3164e4
adnane el yazidi <adanyazi22@outlook.com>
2025-11-13 23:57:11 +0100

    refactor(stepper-form): apply general modifications and improvements

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//DOM Elements
+//DOm elements
 const circles = document.querySelectorAll(".circle"),
     progressBar= document.querySelector(".indicator"),
     buttons = document.querySelectorAll("button");
